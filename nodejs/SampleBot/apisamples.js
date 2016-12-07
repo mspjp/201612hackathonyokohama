1db02c0f294c7d0cbe0fc19be690701f4a35bce4
mmitti <dev@mail.mmitti.info>
2016-12-08 08:52:06 +0900

    ファイル構造を修正

--- nodejs/SampleBot/apisamples.js
+++ nodejs/SampleBot/apisamples.js
@@ -4,7 +4,8 @@ var fs = require('fs');
 var apikeyPath = './apikey.json';
 var API_KEY = JSON.parse(fs.readFileSync(apikeyPath,'utf8'));
 
-var docomo = require("./docomoapi.js")(API_KEY.DOCOMO_APIKEY);
+var Docomo = require("./docomoapi.js")
+var docomo = new Docomo(API_KEY.DOCOMO_APIKEY);
 
 function showDescription(){
     console.log("");
@@ -26,8 +27,40 @@ process.stdin.on('data', function (data) {
     if(data.indexOf("999")!=-1){
         process.exit();
     }else if(data.indexOf("1")!=-1){
-        //形態素解析
-        docomo.morph();
+      docomo.morph('今日の天気は、晴のち曇。もしくは雨か雪。', 'form|pos|read', '', function(r){
+        console.log('今日の天気は、晴のち曇。もしくは雨か雪。:',r);
+      });
+      docomo.morph('猫のキティーが膝の上に乗っている', 'form|pos', '名詞|動詞語幹', function(r){
+        console.log('猫のキティーが膝の上に乗っている:',r);
+      });
+    }else if(data.indexOf("2")!=-1){
+      docomo.hiragana('こにゃにゃちわ〜', 'katakana', function(r){
+        console.log('こにゃにゃちわ〜:',r);
+      });
+      docomo.hiragana('魑魅魍魎', 'hiragana', function(r){
+        console.log('魑魅魍魎:',r);
+      });
+      docomo.hiragana('Step By Step システム構築ガイド', 'hiragana', function(r){
+        console.log('Step By Step システム構築ガイド:',r);
+      });
+    }else if(data.indexOf("3")!=-1){
+      docomo.entity('水野氏が設計した日本株式会社 東京本社の完成記念式典を10月10日　20:15から行う', null, function(r){
+        console.log('「水野氏が設計した日本株式会社 東京本社の完成記念式典を10月10日　20:15から行う」 固有表現抽出:',r);
+      });
+      docomo.entity('横浜ハッカソン@10月10日', 'DAT', function(r){
+        console.log('横浜ハッカソン@10月10日:',r);
+      });
+      docomo.entity('横浜ハッカソン@10月10日', null, function(r){
+        console.log('横浜ハッカソン@10月10日(ALL):',r);
+      });
+    }else if(data.indexOf("4")!=-1){
+      docomo.similarity('Tsukuba', 'つくば', function(s){
+        if(s == -1)console.log('エラー');
+        else console.log('Tsukuba-つくば:類似度:', s);
+      });
+      docomo.similarity('yokkaichi', '四日市', function(s){
+        console.log('yokkaichi-四日市:類似度:', s);
+      });
     }else if(data.indexOf("5")!=-1){
         var image = "http://www.appbank.net/wp-content/uploads/2016/10/koidance-1.jpg";
         cognitive.faceDetect(API_KEY.FACE_APIKEY,image,true,true,"smile",function(error,response,body){
@@ -37,7 +70,7 @@ process.stdin.on('data', function (data) {
                 console.log('error: '+ JSON.stringify(response));
             }
         });
-        
+
     }else{
         console.log("認識していないコマンドです "+data);
     }
--- nodejs/SampleBot/docomoapi.js
+++ nodejs/SampleBot/docomoapi.js
@@ -1,10 +1,81 @@
+var request = require('request');
+var _api_call  = function (key, arg, url, callback){
+  var options = {
+    uri: url + '?APIKEY=' + key,
+    method: 'POST',
+    json: arg,
+    'content-type': 'application/json'
+  };
+  request(options, function(error, response, body){
+    if (!error && response.statusCode == 200) {
+      callback(body);
+    } else {
+      console.log('error: '+ response.statusCode, body);
+      callback();
+    }
+  });
+}
+
 var client = function(apiKey){
     this.apiKey = apiKey;
 
-    this.morph = function(){
-        console.log("execute morph analyze");
-        console.log("your api key is "+this.apiKey);
-    }
+    this.similarity = function (str1, str2, callback) {
+      var result = _api_call(apiKey, {
+        query_pair:[str1, str2]
+      }, 'https://api.apigw.smt.docomo.ne.jp/gooLanguageAnalysis/v1/similarity',
+      function (body){
+        if(body){
+          callback(body.score);
+        }else{
+          callback(-1);
+        }
+      });
+    };
+    this.entity = function (sentence, classFilter, callback) {
+      if(!classFilter){
+        classFilter = 'ART|DAT|LOC|ORG|PSN|TIM';
+      }
+      var result = _api_call(apiKey, {
+        sentence:sentence,
+        class_filter:classFilter
+      }, 'https://api.apigw.smt.docomo.ne.jp/gooLanguageAnalysis/v1/entity',
+      function (body){
+        if(body){
+          callback(body.ne_list);
+        }else{
+          callback(-1);
+        }
+      });
+    };
+
+    this.hiragana = function (sentence, mode, callback) {
+      var result = _api_call(apiKey, {
+        sentence:sentence,
+        output_type: mode
+      }, 'https://api.apigw.smt.docomo.ne.jp/gooLanguageAnalysis/v1/hiragana',
+      function (body){
+        if(body){
+          callback(body.converted);
+        }else{
+          callback(-1);
+        }
+      });
+    };
+
+    this.morph = function (sentence, intoFilter, posFilter, callback) {
+      var result = _api_call(apiKey, {
+        sentence:sentence,
+        info_filter: intoFilter,
+        pos_filter:posFilter
+      }, 'https://api.apigw.smt.docomo.ne.jp/gooLanguageAnalysis/v1/morph',
+      function (body){
+        if(body){
+          callback(body.word_list);
+        }else{
+          callback(-1);
+        }
+      });
+    };
 }
 
 module.exports = client;
