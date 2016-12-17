76cab0520581fca7e132487c9253be92b7a58b8c
mmitti <masashi-asuka@tempo.ocn.ne.jp>
2016-12-17 23:47:53 +0900

    画像だけが添付されたときにTextがnullで落ちるので修正

--- csharp/SampleBot/SampleBot/Controllers/MessagesController.cs
+++ csharp/SampleBot/SampleBot/Controllers/MessagesController.cs
@@ -30,7 +30,7 @@ namespace SampleBot
         {
             //rule.csvに書いてあるルールとマッチングを行う
             //マッチしたルールが2つ以上あると最初のルールが選択される
-            var responses = RuleManager.Instance.SearchResponses(message.Text);
+            var responses = RuleManager.Instance.SearchResponses(message.Text??"");
             foreach(var res in responses)
             {
                 if (res.Contains("{") && res.Contains("}"))
